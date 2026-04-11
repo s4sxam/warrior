8fa1950224f072188d1dfe23a847cb2f99cc9d0f
Tanay ⌬ <tanayguha07@gmail.com>
2026-04-11 17:51:33 +0530

    Update DashboardScreen.kt

--- app/src/main/java/com/tanay/warrior2026/ui/screens/DashboardScreen.kt
+++ app/src/main/java/com/tanay/warrior2026/ui/screens/DashboardScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Skull
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
