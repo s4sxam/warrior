5497230fe0cf97e010ead11cb8a80ace9c20b858
Tanay ⌬ <tanayguha07@gmail.com>
2026-04-26 19:43:59 +0530

    Update GlitchEffect.kt

--- app/src/main/java/com/tanay/warrior2026/ui/components/GlitchEffect.kt
+++ app/src/main/java/com/tanay/warrior2026/ui/components/GlitchEffect.kt
@@ -1,6 +1,6 @@
 package com.tanay.warrior.ui.components
 
-import androidx.compose.animation.core.withFrameMillis
+import androidx.compose.runtime.withFrameMillis
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
